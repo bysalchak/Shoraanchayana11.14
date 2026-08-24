1787e110e7268db7531a271ef2ac39bf78ede475
bysalchak <gcrg95d2pm@privaterelay.appleid.com>
2026-08-24 23:51:54 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const revealElements =
+  document.querySelectorAll(".reveal");
+
+const observer =
+  new IntersectionObserver(
+    entries => {
+
+      entries.forEach(entry => {
+
+        if (
+          entry.isIntersecting
+        ) {
+
+          entry.target
+            .classList
+            .add("visible");
+
+          observer
+            .unobserve(
+              entry.target
+            );
+
+        }
+
+      });
+
+    },
+    {
+      threshold: 0.12
+    }
+  );
+
+revealElements
+  .forEach(element => {
+    observer.observe(element);
+  });
